9556dfda9989861d018482a14429e663f4b359a4
Elliot Weaver <elliot.weaver@gmail.com>
2014-06-28 12:03:40 -0700

    polished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+(function($) {
 
+  $(".page-what .what").on('click', function(){
+
+    var $pop = $(".pop", this);
+
+    if($pop.hasClass('active')){
+
+    }
+
+    // Make Active
+    else {
+      $pop.addClass('active');
+      $pop.animate({
+        top: '0px'
+      },500, function(){
+        // empty
+      });
+    }
+
+  });
+
+  var $close = $(".close");
+  $close.on('click', function(){
+    $parent = $(this).parent();
+    $parent.animate({
+      top: '-1000px'
+    },500, function(){
+      $parent.removeClass('active');
+    });
+  })
+
+}(jQuery));
